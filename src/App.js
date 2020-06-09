3ddf5225dad4eb0a2b4491b3ef7a7c89e69bf7b4
Syed Zubair <syedzubi270@gmail.com>
2020-06-09 10:46:29 -0400

    modified:   public/index.html 	modified:   src/App.css 	modified:   src/App.js 	modified:   src/components/landing.js 	modified:   src/components/projects.js 	new file:   src/components/sample_image.jpg

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import { Layout, Navigation, Drawer, Header, Content } from "react-mdl";
 import Main from "./components/main";
 import { Link } from "react-router-dom";
+import "./App.css";
 
 export class App extends Component {
   render() {
     return (
       <div className='demo-big-content'>
         <Layout>
-          <Header title='Title' scroll>
+          <Header className='header-color' title='Syed Zubair Portfolio' scroll>
             <Navigation>
               <Link to='/aboutme'>About Me</Link>
               <Link to='/projects'>Projects</Link>
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,10 +1,27 @@
 import React, { Component } from "react";
-
+import { Grid, Cell } from "react-mdl";
+//import Avatar from "./sample_image.jpg";
 export class landing extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing Page</h1>
+      <div style={{ width: "100%", margin: "auto" }}>
+        <Grid className='landing-grid'>
+          <Cell col={12}>
+            <img
+              src='https://i.ya-webdesign.com/images/funny-png-avatar-2.png'
+              alt='avatar'
+              className='avatar-img'
+            />
+            <div className='banner-text'>
+              <h1>If i am not </h1>
+              <hr />
+              <p>
+                HTML/CSS | JavaScript | BootStrap | React | React Native |
+                NodeJs | MySQL | MongoDB
+              </p>
+            </div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
